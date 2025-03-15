1f3696ec8c6719ce962ffad7ef8aa55ba21df849
Fabrizio Foglia <fabryfoglia@me.com>
2025-03-15 15:58:18 +0100

    Refactor pom.xml: centralizzazione versioni e miglior gestione dipendenze

---
+++ backend/fantaf1-backend/fantaf1-dati/src/main/java/com/fantaf1/fantaf1_dati/Fantaf1DatiApplication.java
@@ -0,0 +1,13 @@
+package com.fantaf1.fantaf1_dati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fantaf1DatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fantaf1DatiApplication.class, args);
+	}
+
+}
