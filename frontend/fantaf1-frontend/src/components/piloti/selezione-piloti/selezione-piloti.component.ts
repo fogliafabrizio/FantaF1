b4002f25026d73d6824a94b63bcec90adfc5abe5
Fabrizio Foglia <fabryfoglia@me.com>
2025-03-19 18:56:29 +0100

    Little fix

--- frontend/fantaf1-frontend/src/components/piloti/selezione-piloti/selezione-piloti.component.ts
+++ frontend/fantaf1-frontend/src/components/piloti/selezione-piloti/selezione-piloti.component.ts
@@ -95,4 +95,8 @@ export class SelezionePilotiComponent implements OnInit, OnDestroy {
     );
     this.budgetRimanente = 100 - this.totaleCosto;
   }
+
+  isButtonDisabled() {
+    return !this.gara?.scelteAperte;
+  }
 }
---
+++ frontend/fantaf1-frontend/src/models/gare.model.ts
@@ -0,0 +1,11 @@
+export interface Gara {
+  circuitName?: string;
+  country?: string;
+  dateTimeRace?: string;
+  id?: number;
+  raceName?: string;
+  name?: string;
+  scadenzaScelte?: string;
+  scelteAperte?: boolean;
+  urlWikipedia?: string;
+}
