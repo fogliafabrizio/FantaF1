5aa5dff3f9e8336b1d21985a8b5d28e83dad03d4
Fabrizio Foglia <fabryfoglia@me.com>
2025-03-19 13:25:17 +0100

    Prima implementazione FE

--- backend/fantaf1-backend/fantaf1-bff/src/main/java/com/fantaf1/fantaf1_bff/controller/PilotiController.java
+++ backend/fantaf1-backend/fantaf1-bff/src/main/java/com/fantaf1/fantaf1_bff/controller/PilotiController.java
@@ -2,7 +2,7 @@ package com.fantaf1.fantaf1_bff.controller;
 
 import com.fantaf1.fantaf1_bff.service.DatiService;
 import lombok.RequiredArgsConstructor;
-import org.openapitools.api.BffApi;
+import org.openapitools.api.Fantaf1BffApi;
 import org.openapitools.model.Pilota;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +11,13 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class PilotiController implements BffApi {
+public class PilotiController implements Fantaf1BffApi {
 
     private final DatiService datiService;
 
     @Override
     public ResponseEntity<Pilota> getPilotaByIdFromDati(Integer id) {
-        return BffApi.super.getPilotaByIdFromDati(id);
+        return Fantaf1BffApi.super.getPilotaByIdFromDati(id);
     }
 
     @Override
--- frontend/fantaf1-frontend/src/app/app.component.ts
+++ frontend/fantaf1-frontend/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Fantaf1BffService } from '../service/fantaf1-bff.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'fantaf1-frontend';
+export class AppComponent implements OnInit {
+  constructor(private readonly _bffService: Fantaf1BffService) {}
+
+  ngOnInit(): void {
+    this._bffService.getDriversByYear(2021).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
